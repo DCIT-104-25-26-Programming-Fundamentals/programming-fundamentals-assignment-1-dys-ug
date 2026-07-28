16a7edf51d7c6fae670b380d124cd24dbb661b1f
drezylord <drezylord@gmail.com>
2026-07-28 19:19:13 +0000

    I coded a program to determine a student's graded based on the provided grading scale

--- assignment_02_student_grade_system.py
+++ assignment_02_student_grade_system.py
@@ -45,3 +45,38 @@
 # YOUR CODE BELOW — remove the # symbols from the scaffold and fill it in
 # =============================================================================
 
+def calculate_grade(student_score):
+
+    if student_score < 0 or student_score > 100:
+        return None
+
+    elif student_score >= 80:
+        return "A"
+
+    elif student_score >= 70:
+        return "B"
+
+    elif student_score >= 60:
+        return "C"
+
+    elif student_score >= 50:
+        return "D"
+
+    else:
+        return "F"
+
+
+def main():
+
+    marks = int(input("Enter student score (0-100): "))
+
+    final_grade = calculate_grade(marks)
+
+    if final_grade is None:
+        print("Error: Score must be between 0 and 100.")
+
+    else:
+        print("Grade:", final_grade)
+
+
+main()
